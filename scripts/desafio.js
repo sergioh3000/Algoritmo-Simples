8d659bf8dee030486dc9622ba7824d163cac7574
Sergio Lima <sergiohenrique300@gmail.com>
2023-09-14 21:32:40 -0300

    algoritmo simples

---
+++ scripts/desafio.js
@@ -0,0 +1,19 @@
+//Nome + olá
+let nome = prompt("Qual seu nome?")
+let resultadoNome = nome
+
+console.log(nome)
+alert("Olá, " + nome)
+
+//idade
+let anoAtual = 2023
+let idade = prompt("Quantos anos você tem?")
+let valorIdade = parseInt(idade)
+
+console.log(idade)
+
+
+// Resultado
+let nascimento = anoAtual - idade
+console.log(nascimento)
+alert("Você nasceu em " + nascimento )
---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let nome = "Sergio"
+let sobrenome = "Lima"
+let anoAtual = 2023
+let idade = 25
+
+// nome e sobrenome
+let resultadoNome = nome + " " + sobrenome
+console.log (resultadoNome)
+
+// nascimento
+let resultadoAno = anoAtual - idade
+console.log (resultadoAno)
+
+// prompt
+let nomeInserido = prompt("insera seu nome")
+console.log(nomeInserido)
+
+// parse transforma o numero digitado em numeral
+let stringNumero = prompt("insira sua idade")
+let valorNumero = parseInt(stringNumero)
+console.log(valorNumero)
+
